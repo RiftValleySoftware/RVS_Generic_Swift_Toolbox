2e185a55abb6690c537d3973402bf53e3360eebd
Chris Marshall <chris@riftvalleysoftware.com>
2020-01-27 14:15:05 -0500

    Just added the sample set from the function header as the Int test.

--- tests/RVS_Extensions_Tests.swift
+++ tests/RVS_Extensions_Tests.swift
@@ -31,8 +31,30 @@ import XCTest
 class RVS_Int_Extensions_Tests: XCTestCase {
     /* ################################################################## */
     /**
+     This is the maskedValue() extension function test.
      */
-    func testSimpleInstantiation() {
+    func testMask() {
+        let targetTest = 3888 // 0xF30 (Value, in hex) 111100110000 (Value, in binary)
+       // 111111111111 (Mask, in binary)
+       XCTAssertEqual(3888, targetTest.maskedValue(firstPlace: 0, runLength: 12))
+       // 111111110000
+       XCTAssertEqual(243, targetTest.maskedValue(firstPlace: 4, runLength: 8))
+       // 000000000011
+       XCTAssertEqual(0, targetTest.maskedValue(firstPlace: 0, runLength: 2))
+       // 000000111100
+       XCTAssertEqual(12, targetTest.maskedValue(firstPlace: 2, runLength: 4))
+       // 000111100000
+       XCTAssertEqual(9, targetTest.maskedValue(firstPlace: 5, runLength: 4))
+       // 011111111111
+       XCTAssertEqual(1840, targetTest.maskedValue(firstPlace: 0, runLength: 11))
+       // 111111111110
+       XCTAssertEqual(1944, targetTest.maskedValue(firstPlace: 1, runLength: 11))
+       // 000000110000
+       XCTAssertEqual(3, targetTest.maskedValue(firstPlace: 4, runLength: 2))
+       // 000001100000
+       XCTAssertEqual(1, targetTest.maskedValue(firstPlace: 5, runLength: 2))
+       // 000011000000
+        XCTAssertEqual(0, targetTest.maskedValue(firstPlace: 6, runLength: 2))
     }
 }
 

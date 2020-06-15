b12f1d1e7005a02d9e9a647e9f42e5efe4b91be4
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-15 06:33:21 -0400

    I added targets fgor each OS. Let's see if this works...

--- Package.swift
+++ Package.swift
@@ -5,13 +5,29 @@ let package = Package(
     products: [
         .library(
             name: "RVS_Generic_Swift_Toolbox",
-            targets: ["RVS_Generic_Swift_Toolbox_iOS"]
+            targets: [  "RVS_Generic_Swift_Toolbox_iOS",
+                        "RVS_Generic_Swift_Toolbox_MacOS",
+                        "RVS_Generic_Swift_Toolbox_WatchOS",
+                        "RVS_Generic_Swift_Toolbox_TVOS"
+            ]
         )
     ],
     targets: [
         .target(
             name: "RVS_Generic_Swift_Toolbox_iOS",
             path: "RVS_Generic_Swift_Toolbox_iOS"
+        ),
+        .target(
+            name: "RVS_Generic_Swift_Toolbox_MacOS",
+            path: "RVS_Generic_Swift_Toolbox_MacOS"
+        ),
+        .target(
+            name: "RVS_Generic_Swift_Toolbox_WatchOS",
+            path: "RVS_Generic_Swift_Toolbox_WatchOS"
+        ),
+        .target(
+            name: "RVS_Generic_Swift_Toolbox_TVOS",
+            path: "RVS_Generic_Swift_Toolbox_TVOS"
         )
     ],
     swiftLanguageVersions: [

7f35ab6f8f7c705082c380b12ed0e2c082bbe332
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-19 13:09:37 -0400

    OK. Let's see if this does it...

--- Package.swift
+++ Package.swift
@@ -4,24 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "RVS_Generic_Swift_Toolbox",
-    platforms: [
-        .macOS(.v10_14),
-        .iOS(.v12),
-        .watchOS(.v5),
-        .tvOS(.v12)
-    ],
     products: [
         .library(
             name: "RVS_Generic_Swift_Toolbox",
             type: .dynamic,
-            targets: [  "RVS_Generic_Swift_Toolbox" ]
-        )
+            targets: ["RVS_Generic_Swift_Toolbox"]),
     ],
     targets: [
         .target(
             name: "RVS_Generic_Swift_Toolbox",
-            path: "src"
-        )
-    ],
-    swiftLanguageVersions: [.v5]
+            path: "./src")
+    ]
 )

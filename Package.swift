c0da6693a7f3c41f1f7e7db981d54d3f6d47a9e0
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-19 10:32:42 -0400

    Sigh...I can see this going on for a while...

--- Package.swift
+++ Package.swift
@@ -4,39 +4,19 @@ import PackageDescription
 
 let package = Package(
     name: "RVS_Generic_Swift_Toolbox",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v11)
+    ],
     products: [
         .library(
-            name: "RVS_Generic_Swift_Toolbox_iOS",
-            targets: [  "RVS_Generic_Swift_Toolbox_iOS" ]
-        ),
-        .library(
-            name: "RVS_Generic_Swift_Toolbox_MacOS",
-            targets: [  "RVS_Generic_Swift_Toolbox_MacOS" ]
-        ),
-        .library(
-            name: "RVS_Generic_Swift_Toolbox_WatchOS",
-            targets: [  "RVS_Generic_Swift_Toolbox_WatchOS" ]
-        ),
-        .library(
-            name: "RVS_Generic_Swift_Toolbox_TVOS",
-            targets: [  "RVS_Generic_Swift_Toolbox_TVOS" ]
+            name: "RVS_Generic_Swift_Toolbox",
+            targets: [  "RVS_Generic_Swift_Toolbox" ]
         )
     ],
     targets: [
         .target(
-            name: "RVS_Generic_Swift_Toolbox_iOS",
-            path: "src"
-        ),
-        .target(
-            name: "RVS_Generic_Swift_Toolbox_MacOS",
-            path: "src"
-        ),
-        .target(
-            name: "RVS_Generic_Swift_Toolbox_WatchOS",
-            path: "src"
-        ),
-        .target(
-            name: "RVS_Generic_Swift_Toolbox_TVOS",
+            name: "RVS_Generic_Swift_Toolbox",
             path: "src"
         )
     ]

12dd9e6e25b482ee93d09bc7c63180f8be276fe5
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-21 14:28:20 -0400

    I'm making the library static, instead of dynamic, to see if that fixes anything.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,6 @@ let package = Package(
     products: [
         .library(
             name: "RVS-Generic-Swift-Toolbox",
-            type: .dynamic,
             targets: ["RVS_Generic_Swift_Toolbox"])
     ],
     targets: [
